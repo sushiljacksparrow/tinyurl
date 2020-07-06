718a938a7d687d3df2da6789d3abc23175f1b2f3
Sushil Singh <sushils@amazon.com>
2020-07-06 18:06:26 -0400

    Use Docker-Compose to build application and storage
    Introducing docker-compose to create all the infrastructure. Read
    properties from .env file instead of hard-coded values into code base.
    Use postgres instead of SQL for storage

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/jinzhu/gorm v1.9.14
+	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.0
 )
--- models/setup.go
+++ models/setup.go
@@ -1,15 +1,24 @@
 package models
 
 import (
+	"fmt"
 	"github.com/jinzhu/gorm"
-	_ "github.com/mattn/go-sqlite3"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 var DB *gorm.DB
 
 func ConnectDataBase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+	host := goDotEnvVariable("DB_HOST")
+	dbPort := goDotEnvVariable("DB_PORT")
+	dbName := goDotEnvVariable("DB_NAME")
+	dbPassword := goDotEnvVariable("DB_PASSWORD")
+	dbUser := goDotEnvVariable("DB_USER")
+	database, err := gorm.Open("postgres", fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s",
+		host, dbPort, dbUser, dbPassword, dbName))
 	if err != nil {
 		log.Panic("Failed to connect to DB", err)
 		panic("Failed to connect to database!")
@@ -18,4 +27,19 @@ func ConnectDataBase() {
 	database.AutoMigrate(&URL{})
 
 	DB = database
+}
+
+
+// use godot package to load/read the .env file and
+// return the value of the key
+func goDotEnvVariable(key string) string {
+
+	// load .env file
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
 }
